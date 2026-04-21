7de3199b33d5ea2e91ea93c140506a3c93f07069
Borozdov <dev@borozdov.ru>
2026-04-21 07:06:49 +0300

    feat: switch Telegram bot to kmevermveokBot

--- eduplatform/src/components/TelegramLinkCard.tsx
+++ eduplatform/src/components/TelegramLinkCard.tsx
@@ -33,7 +33,7 @@ export const TelegramLinkCard = () => {
     const s = document.createElement('script');
     s.src = 'https://telegram.org/js/telegram-widget.js?23';
     s.async = true;
-    s.setAttribute('data-telegram-login', 'Swimming_Coaches_Association_bot');
+    s.setAttribute('data-telegram-login', 'kmevermveokBot');
     s.setAttribute('data-size', 'large');
     s.setAttribute('data-onauth', 'onTelegramAuth(user)');
     s.setAttribute('data-request-access', 'write');
--- eduplatform/src/pages/Auth.tsx
+++ eduplatform/src/pages/Auth.tsx
@@ -76,7 +76,7 @@ export const Auth = () => {
     const script = document.createElement('script');
     script.src = 'https://telegram.org/js/telegram-widget.js?23';
     script.async = true;
-    script.setAttribute('data-telegram-login', 'Swimming_Coaches_Association_bot');
+    script.setAttribute('data-telegram-login', 'kmevermveokBot');
     script.setAttribute('data-size', 'large');
     script.setAttribute('data-onauth', 'onTelegramAuth(user)');
     script.setAttribute('data-request-access', 'write');
